00d58169380045b600ef868959e7fbd197ecb34e
swan484 <swan484@uwo.ca>
2021-01-16 12:23:22 -0500

    Got rid of unused lines in app.py. Connected to index.html

--- app.py
+++ app.py
@@ -7,43 +7,10 @@ PICTURE_FOLDER = os.path.join('static', 'picture_photo')
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = PICTURE_FOLDER
 
-from pathplanner import Session
-
-# @app.route('/', methods=['GET'])
-# def api():
-# return {
-# 'userId':1,
-# 'title': 'Flask React Application',
-# 'completed': False
-# }
 @app.route('/')
 def home():
-    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'explore.png')
-    return render_template("home.html", user_image = full_filename)
-
-
-@app.route("/questions", methods=['GET', 'POST'])
-def questions():
-    if request.method == 'POST':
-        return redirect(url_for('home'))
-
-    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'explore.png')
-    return render_template('questions.html', user_image = full_filename)
-
-
-@app.route("/index", methods=['GET', 'POST'])
-def index():
-    locations = []
-    #if request.method == 'GET':
-     #   locations = ["54 clifford fairbarn dr, ONT", "36 peter miller st, ONT", "12 christie str, ONT"]
-
-    if request.method == 'POST':
-        distance = request.form['distance']
-        sights = request.form.getlist('sight')
-        locations = Session(sights,distance).main()
-
-    full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'download.png')
-    return render_template('index.html', user_image = full_filename, locations=locations)
+    
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
