b9810f80aa1be16e6b3969dadc6a7824a6251b38
l-maier <lmaier@uwaterloo.ca>
2021-01-16 17:54:44 -0500

    added generic code for login page (currently is a different page then the landing page)

--- app.py
+++ app.py
@@ -21,6 +21,17 @@ def home():
     
     return render_template("index.html")
 
+    # Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost',port=5001)
