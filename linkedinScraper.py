0dea0c35f37f4791d579bdfb09456d11382a5c05
l-maier <lmaier@uwaterloo.ca>
2021-01-16 20:39:25 -0500

    no longer needs you to specify direct path of driver

--- linkedinScraper.py
+++ linkedinScraper.py
@@ -1,10 +1,14 @@
 from linkedin_scraper import Person, actions
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
 
 class LinkedinScraper:
 
     def __init__(self, email, password):
-        self.driver = webdriver.Chrome('C:/Western/LinkedinRoast/htn2020/chromedriver.exe')
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        #self.driver = webdriver.Chrome('C:/Users/Frienddo/Desktop/htn2020/chromedriver.exe')
         actions.login(self.driver, email, password) # if email and password isnt given, it'll prompt in terminal
         self.person = None
     
