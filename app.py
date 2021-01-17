3f8310fdc6aabcdcf3440766baff4041f594570d
rprakitpong <rprakitpong@gmail.com>
2021-01-16 20:58:01 -0800

    state machine

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ import os
 from linkedinScraper import LinkedinScraper
 
 app = Flask(__name__)
-
+state = -1
+has_question = {}
+question = {}
+yes_answer = {}
+no_answer = {}
 
 @app.route('/')
 def home():  
     return render_template("index.html")
 
 @app.route('/go', methods=["POST"])
-
 def letsgo():
     usr_email = request.form.get('email')
     usr_password = request.form.get('password')
@@ -25,10 +28,44 @@ def letsgo():
     title = scrapp.getTitle()
 
     if about != '':
-        return render_template('questions.html', question='huh some smug question', yes='yes', no='no')
+        has_question[0] = True
+        question[0] = ''
+        yes_answer[0] = ''
+        no_answer[0] = ''
     else:
-        pass
-        return 'yya'
+        has_question[0] = False
+    
+    if edu != '':
+        has_question[1] = True
+        ## copy paste this for everything
+    else:
+        has_question[1] = False
+    
+    state = -1
+    question[-1] = 'Are you ready?'
+    yes_answer[-1] = 'Yes...'
+    no_answer[-1] = 'No but do I have a choice?'
+    return render_template('questions.html', question=question[state], yes=yes_answer[state], no=[state])
+
+
+@app.route('/huh', methods=["POST"])
+def question():
+    """
+    if request.form['answer'] == yes_answer[state]:
+        return 'wow yes'
+    if request.form['answer'] == no_answer[state]:
+        return 'wow no'
+    return 'huh ?'
+    """
+    state = state + 1
+    while state < 4 and has_question[state] == False:
+        state = state + 1
+
+    if state < 4:
+        return render_template('questions.html', question=question[state], yes=yes_answer[state], no=[state])
+    else:
+        return render_template('ending.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost',port=5001)
